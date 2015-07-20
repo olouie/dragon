f3ec422cecfda18077a531a4b059521c0294c8e0
olouie <olivia.louie@gmail.com>
2015-07-19 22:06:18 -0700

    Bob is alive

--- dragon.rb
+++ dragon.rb
@@ -1,3 +1,30 @@
-Class Dragon
+class Dragon
+
+  def initialize(name)
+  	@name = name
+  	@hunger = 2
+  	@sleepy = false
+
+    puts "#{@name} has hatched!"
+  end
+
+  def feed
+  	puts "You fed #{@name} a piece of meat."
+
+  	if @hunger < 4
+  	  puts "#{@name} still looks kind of hungry."
+  	else
+  	  puts "#{@name} looks very full!"
+  	end
+
+  	@hunger += 1
+  end
+
+  def play()
+
+  end
 
 end
+
+pet = Dragon.new('Bob')
+pet.feed
